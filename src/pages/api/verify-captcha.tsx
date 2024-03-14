6d6de55ab7a5a9d602309348d1eee9686232931d
unknown <alexander.makarov62@gmail.com>
2024-03-14 12:44:05 +1100

    added even more logging

--- src/pages/api/verify-captcha.tsx
+++ src/pages/api/verify-captcha.tsx
@@ -1,10 +1,21 @@
 import axios from 'axios';
 
+console.log('Environment Variables:', Object.keys(process.env));
+
 export default async function handler(req, res) {
   const { token } = req.body;
   console.log('Received token:', token);
 
-  // Check if the RECAPTCHA_SECRET_KEY is set without logging it
+  // Check if the NEXT_PUBLIC_RECAPTCHA_SITE_KEY is set and log it
+  if (!process.env.NEXT_PUBLIC_RECAPTCHA_SITE_KEY) {
+    console.error('NEXT_PUBLIC_RECAPTCHA_SITE_KEY is not set.');
+    res.status(500).json({ error: 'NEXT_PUBLIC_RECAPTCHA_SITE_KEY is not set' });
+    return;
+  } else {
+    console.log('NEXT_PUBLIC_RECAPTCHA_SITE_KEY:', process.env.NEXT_PUBLIC_RECAPTCHA_SITE_KEY);
+  }
+
+  // Continue with RECAPTCHA_SECRET_KEY checks and reCAPTCHA verification as before
   if (!process.env.RECAPTCHA_SECRET_KEY) {
     console.error('RECAPTCHA_SECRET_KEY is not set.');
     res.status(500).json({ error: 'RECAPTCHA_SECRET_KEY is not set' });
@@ -16,8 +27,6 @@ export default async function handler(req, res) {
       `https://www.google.com/recaptcha/api/siteverify?secret=${process.env.RECAPTCHA_SECRET_KEY}&response=${token}`
     );
     console.log('Verification request to Google reCAPTCHA API successful.');
-    // Log the response data except the secret key
-    console.log('Google API response:', response.data);
     res.status(200).json(response.data);
   } catch (error) {
     console.error('Error during reCAPTCHA verification:', error);
